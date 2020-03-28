719c8a6246915a39338c85efb611c13bcb0b5961
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-03-27 21:21:16 -0700

    Add files via upload

---
+++ Scripts/Karen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Karen : MonoBehaviour
+{
+    bool executingBehavior = false;
+    Task currentTask;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!executingBehavior)
+            {
+                executingBehavior = true;
+                currentTask = BuildTask();
+
+                EventBus.StartListening(currentTask.TaskFinished, OnTaskFinished);
+                currentTask.run();
+            }
+        }
+    }
+
+    void OnTaskFinished()
+    {
+        EventBus.StopListening(currentTask.TaskFinished, OnTaskFinished);
+        executingBehavior = false;
+    }
+
+    Task BuildTask()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        List<Task> taskList = new List<Task>();
+
+        Task wait = new Wait(1f);
+        Task turnRed = new TurnRed(renderer);
+        Task turnYellow = new TurnYellow(renderer);
+        Task turnBlue = new TurnBlue(renderer);
+
+        taskList.Add(turnRed);
+        taskList.Add(wait);
+        taskList.Add(turnYellow);
+        taskList.Add(wait);
+        taskList.Add(turnBlue);
+
+        Sequence sequence = new Sequence(taskList);
+
+        taskList = new List<Task>();
+
+        taskList.Add(sequence);
+
+        Selector selector = new Selector(taskList);
+
+        return selector;
+    }
+}
---
+++ Scripts/Task.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Task
+{
+    public abstract void run();
+    public bool succeeded;
+
+    protected int eventId;
+    const string EVENT_NAME_PREFIX = "FinishedTask";
+
+    public string TaskFinished
+    {
+        get
+        {
+            return EVENT_NAME_PREFIX + eventId;
+        }
+    }
+    public Task()
+    {
+        eventId = EventBus.GetEventID();
+    }
+}
+
+public class TurnRed : Task
+{
+    Renderer color;
+
+    public TurnRed(Renderer renderer)
+    {
+        color = renderer;
+    }
+
+    public override void run()
+    {
+        color.material.color = new Color(255, 0, 0);
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class TurnYellow : Task
+{
+    Renderer color;
+
+    public TurnYellow(Renderer renderer)
+    {
+        color = renderer;
+    }
+
+    public override void run()
+    {
+        color.material.color = new Color(255, 128, 0);
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class TurnBlue : Task
+{
+    Renderer color;
+
+    public TurnBlue(Renderer renderer)
+    {
+        color = renderer;
+    }
+
+    public override void run()
+    {
+        color.material.color = new Color(0, 0, 255);
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class Wait : Task
+{
+    float waitTime;
+
+    public Wait(float time)
+    {
+        waitTime = time;
+    }
+
+    public override void run()
+    {
+        succeeded = true;
+        EventBus.ScheduleTrigger(TaskFinished, waitTime);
+    }
+}
+
+public class Sequence : Task
+{
+    List<Task> children;
+    Task currentTask;
+    int currentTaskIndex = 0;
+
+    public Sequence(List<Task> taskList)
+    {
+        children = taskList;
+    }
+
+    public override void run()
+    {
+        currentTask = children[currentTaskIndex];
+        EventBus.StartListening(currentTask.TaskFinished, OnChildTaskFinished);
+        currentTask.run();
+    }
+
+    void OnChildTaskFinished()
+    {
+        if (currentTask.succeeded)
+        {
+            EventBus.StopListening(currentTask.TaskFinished, OnChildTaskFinished);
+            currentTaskIndex++;
+            if (currentTaskIndex < children.Count)
+            {
+                this.run();
+            }
+            else
+            {
+                succeeded = true;
+                EventBus.TriggerEvent(TaskFinished);
+            }
+
+        }
+        else
+        {
+            succeeded = false;
+            EventBus.TriggerEvent(TaskFinished);
+        }
+    }
+}
+
+public class Selector : Task
+{
+    List<Task> children;
+    Task currentTask;
+    int currentTaskIndex = 0;
+
+    public Selector(List<Task> taskList)
+    {
+        children = taskList;
+    }
+
+    public override void run()
+    {
+        currentTask = children[currentTaskIndex];
+        EventBus.StartListening(currentTask.TaskFinished, OnChildTaskFinished);
+        currentTask.run();
+    }
+
+    void OnChildTaskFinished()
+    {
+        if (currentTask.succeeded)
+        {
+            succeeded = true;
+            EventBus.TriggerEvent(TaskFinished);
+        }
+        else
+        {
+            EventBus.StopListening(currentTask.TaskFinished, OnChildTaskFinished);
+            currentTaskIndex++;
+            if (currentTaskIndex < children.Count)
+            {
+                this.run();
+            }
+            else
+            {
+                succeeded = false;
+                EventBus.TriggerEvent(TaskFinished);
+            }
+        }
+    }
+}
+
+
+
+
